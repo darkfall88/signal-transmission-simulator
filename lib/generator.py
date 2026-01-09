9fe59057ffb81b00be7132e39a3d10b9085328b6
kuribam <arbororvickcz@gmail.com>
2026-01-09 11:26:58 +0100

    refactoring - presunuti UI do lib souboru

--- lib/generator.py
+++ lib/generator.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def generate_binary_signal(Rb=10_000, frame_length=1000, seed=12345, samples_per_bit=20):
     """
@@ -26,5 +27,13 @@ def generate_binary_signal(Rb=10_000, frame_length=1000, seed=12345, samples_per
     t = np.arange(len(nrz)) / fs
     return bits, nrz, t, fs, Rb
 
-
-
+def plot_binary_signal(bits, nrz, t, Rb):
+    plt.figure(figsize=(12, 4))
+    plt.step(t, nrz, where="post")
+    plt.ylim(-1.2, 1.2)
+    plt.xlabel("Čas [s]")
+    plt.ylabel("Amplituda")
+    plt.title(f"Telegrafní NRZ signál (Rb = {Rb/1e3:.1f} kb/s)")
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
